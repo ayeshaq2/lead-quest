62c446ca9dde98d21becd72f7954215090b7691f
Ayesha <aqaisar3@uwo.ca>
2023-01-14 16:01:42 -0500

    7th

--- src/components/Int1.js
+++ src/components/Int1.js
@@ -6,8 +6,10 @@ export default function Int(){
     return (
 
         <div className='int--body'>
-            <section className='int--section'><h5 className='int--question'>speec</h5></section>
-            <button className='start--btn'> Start </button>
+            <section className='int--section'><h5 className='int--question'> Q1: what is the question? </h5>
+            <h6 className='int--text'>Speech to text</h6>
+            <button className='start--btn'> Start </button></section>
+            
         </div>
 
 
