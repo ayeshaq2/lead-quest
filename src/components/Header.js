d14356f7dad330129cfe6a19156b5c179e03557f
Ayesha <aqaisar3@uwo.ca>
2023-01-14 14:20:12 -0500

    second

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { useNavigate } from 'react-router-dom'
+import Button from './Button1'
+
+export default function Header(){
+ 
+
+    return(
+        <div>
+
+
+
+        </div>
+    )
+
+
+}
---
+++ src/components/Int1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function Int(){
+    return (
+
+        <nav>
+            <h5>question goes here</h5>
+        </nav>
+
+
+
+    )
+}
