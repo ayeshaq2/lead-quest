aa6b0b443c132f895f0bd128e6286983951294b4
Ayesha <aqaisar3@uwo.ca>
2023-01-14 15:40:26 -0500

    fifth

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { useNavigate } from 'react-router-dom'
 import Button from './Button1'
-import logo from './images/airbnb.png'
+import logo from './images/logo2.png'
+
 
 export default function Header(){
- 
-
     return(
-        <div>
-            <span><img src={logo}></img>
-            <h5 className='header'>question goes here</h5></span>
+        <nav className='navbar'>
+            <span><img src={logo} className="header--image"></img></span>
+            <h5 className='header'>question goes here</h5>
 
-        </div>
+        </nav>
     )
 
 
--- src/components/Int1.js
+++ src/components/Int1.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
 export default function Int(){
     return (
 
-        <nav>
+        <div className='int--body'>
             <section className='int--section'>speech to text</section>
-            <button className='start--btn'>Start</button>
-        </nav>
+            <button className='start--btn'> Start </button>
+        </div>
 
 
 
