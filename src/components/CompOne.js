5e8793e1bc1ed0c1988a0067fb9eb76686e927c3
Ayesha <aqaisar3@uwo.ca>
2023-01-14 11:46:41 -0500

    first

---
+++ src/components/CompOne.js
@@ -0,0 +1,18 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import {useNavigate} from "react-router-dom";
+import lesson1 from "./lesson1"
+
+export default function MainOne(){
+    
+    const navigate = useNavigate();
+    return(
+    
+        <main>
+            <button className="btn1" >button 1</button>
+            <button className="ntn2">button2</button>
+            <button className = "f" onClick={()=>navigate('./lesson1',{replace:true})}> first class</button>
+-
+        </main>
+    )
+}
---
+++ src/components/lesson1.js
@@ -0,0 +1,19 @@
+import React from "react"
+import logo from "./images/airbnb.png"
+import comp from "./CompOne"
+
+
+
+export default function lessonOne(){
+    
+   
+    return(
+    
+        <main>
+            <button className="btn1" >button 1</button>
+            <button className="ntn2">button2</button>
+            
+-
+        </main>
+    )
+}
