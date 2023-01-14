76c29868bb4ab908e784283c0f183af21766c380
Ayesha <aqaisar3@uwo.ca>
2023-01-14 14:28:16 -0500

    second

---
+++ src/components/comp2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function comptwo() {
+    return(
+        <div>
+            <h2>you made it</h2>
+            <button>third button</button>
+        </div>
+    )
+}
